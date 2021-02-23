a7d6075626e4b1ce58bacba704be5ba7ea9ccd59
Dominic Létourneau <doumdi@gmail.com>
2021-02-23 10:30:04 -0500

    Testing docker and volumes

--- start_docker.sh
+++ start_docker.sh
@@ -7,7 +7,7 @@ echo "Sleeping 5 secs."
 sleep 5
 echo "Starting TeraServer"
 (cd /opentera/teraserver/python && $PYTHON3_EXEC ./TeraServer.py&)
-echo "Sleeping 5 secs."
-sleep 5
+echo "Sleeping 15 secs before launching opentera-teleop-service."
+sleep 15
 echo "Starting opentera-teleop-service"
 (cd /opentera-teleop-service && $TELEOP_PYTHON3_EXEC ./TeleopService.py)
