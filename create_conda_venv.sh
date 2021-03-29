7bc4552a8a6809789188866afe93c280a57bfd85
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2021-03-29 10:56:40 -0400

    Installing all requirements

--- create_conda_venv.sh
+++ create_conda_venv.sh
@@ -1,1 +1,8 @@
+echo "Create or update conda venv"
 conda install -m -c conda-forge -y -p $PWD/venv python=3.8
+echo "Activating venv"
+conda activate $PWD/venv
+echo "Installing requirements (make sure git submodules are installed)..."
+pip install -r $PWD/requirements.txt
+pip install -r $PWD/submodules/opentera-webrtc/signaling-server/requirements.txt
+
