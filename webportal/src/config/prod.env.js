2af996532ae693faa5fb72ea0041877405735666
Dominic Létourneau <doumdi@gmail.com>
2021-10-15 11:26:04 -0400

    Testing env

---
+++ webportal/src/config/prod.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  VUE_APP_BASE_URL: '"https://telesante.3it.usherbrooke.ca/robot/static"',
+  OPENTERA_API_URL: '"https://telesante.3it.usherbrooke.ca:40075/"'
+}
--- webportal/vue.config.js
+++ webportal/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
-  }
+  },
+  outputDir: 'dist',
+  assetsDir: 'static',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/robot/'
+    : '/'
 }
