c6c37f5e4d2850e897b3f274f6f059132adaf545
Dominic Létourneau <doumdi@gmail.com>
2021-11-15 12:00:47 -0500

    Added translations

--- webportal/src/components/About.vue
+++ webportal/src/components/About.vue
@@ -1,41 +1,40 @@
 <template>
-      <div class="d-grid container-fluid bg-white">
-            <div class="row">
-                  <div class="col-md-12">
-                        <h1 class="bg-info">Robot Teleoperation Service</h1>
-                  </div>
-            </div>
+      <div class="container-fluid bg-white">
+            <div class="row my-0">
 
-            <div class="row mt-1">
-                  <h3 class="mt-4 bg-info">Project Page</h3>
-                  <div class="col-md-12">
-                        <a href="https://github.com/introlab/opentera-teleop-service" target="window" class="link-info">
-                        <img src="@/assets/GitHub.png" alt="GitHub" class="img-fluid mx-2" width="300">
-                        </a>
-                  </div>
+                  <h1 class="bg-info">{{ $t('Robot Teleoperation Service') }}</h1>
+                  <div class="row mx-1">
 
-                  <h3 class="mt-4 bg-info">Powered By</h3>
-                  <div class="col-md-12">
-                  <a href="https://github.com/introlab/opentera" target="window" class="link-info">
-                  <img src="@/assets/LogoOpenTera.png" alt="3IT" class="img-fluid mx-2" width="200">
-                  </a>
-                  </div>
+                        <h3 class="my-4 bg-info">{{ $t('Project Page') }}</h3>
+                        <div class="col-md-12">
+                              <a href="https://github.com/introlab/opentera-teleop-service" target="window" class="link-info">
+                              <img src="@/assets/GitHub.png" alt="GitHub" class="img-fluid mx-2" width="300">
+                              </a>
+                        </div>
 
-                  <h3 class="mt-5 bg-info">Acknowledgements</h3>
-                  <div class="col-md-12 mt-4">
-                        <a href="https://agewell-nce.ca/" target="window" class="link-info">
-                        <img src="@/assets/AgeWell.png" alt="3IT" class="img-fluid mx-4" width="300">
-                        </a>
-                        <a href="https://introlab.3it.usherbrooke.ca" target="window" class="link-info">
-                        <img src="@/assets/IntRoLab.png" alt="3IT" class="img-fluid mx-2" width="300">
-                        </a>
-                        <a href="https://usherbrooke.ca/3it" target="window" class="link-info">
-                        <img src="@/assets/3IT.png" alt="3IT" class="img-fluid mx-2" width="300">
-                        </a>
+                        <h3 class="my-4 bg-info">{{ $t('Powered By') }}</h3>
+                        <div class="col-md-12">
+                              <a href="https://github.com/introlab/opentera" target="window" class="link-info">
+                              <img src="@/assets/LogoOpenTera.png" alt="3IT" class="img-fluid mx-2" width="200">
+                              </a>
+                        </div>
+
+                        <h3 class="my-5 bg-info">{{ $t('Acknowledgements') }}</h3>
+                        <div class="col-md-12 mt-4">
+                              <a href="https://agewell-nce.ca/" target="window" class="link-info">
+                              <img src="@/assets/AgeWell.png" alt="3IT" class="img-fluid mx-4" width="300">
+                              </a>
+                              <a href="https://introlab.3it.usherbrooke.ca" target="window" class="link-info">
+                              <img src="@/assets/IntRoLab.png" alt="3IT" class="img-fluid mx-2" width="300">
+                              </a>
+                              <a href="https://usherbrooke.ca/3it" target="window" class="link-info">
+                              <img src="@/assets/3IT.png" alt="3IT" class="img-fluid mx-2" width="300">
+                              </a>
+                        </div>
+                        <p class="fst-italic mt-4 text-end">
+                        Version: 1.0.0
+                        </p>
                   </div>
-                  <p class="fst-italic mt-4 text-end">
-                  Version: 1.0.0
-                  </p>
             </div>
       </div>
 </template>
